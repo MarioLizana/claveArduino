cf0638a943419c98f50c645f9318f295d29c827e
Mario <marviclizana@gmail.com>
2013-12-17 11:08:57 +0100

    Version 1.0. Programa funcionando

--- codeArduino/codeArduino.ino
+++ codeArduino/codeArduino.ino
@@ -1,3 +1,21 @@
+/******************************************************************************/
+/**                   Proyecto final curso de Arduino                        **/
+/**                                                                          **/
+/**       Sistema de alarma con:     Clave en teclado matricial de 4x4       **/
+/**                                  Menu en display LCD                     **/ 
+/**                                  Detector de movimiento por infrarojos   **/
+/**                                                                          **/
+/**                                                                          **/
+/**       Creador:         MARIO LIZANA CALVO                                **/
+/**                                                                          **/
+/**       Licencias:     http://www.opensource.org/licenses/mit-license.php  **/
+/**                      http://www.tapr.org/OHL                             **/
+/******************************************************************************/
+
+/*----------------------------------------------------------------------------*/
+/*-                     Librerias usadas y variables                         -*/
+/*----------------------------------------------------------------------------*/
+
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
@@ -11,11 +29,8 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {6,7,8,9}; //connect to row pinouts 
-byte colPins[COLS] = {10,11,12,13}; //connect to column pinouts
-
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-
+byte rowPins[ROWS] = {6,7,8,9};                 //Pines de las columnas
+byte colPins[COLS] = {10,11,12,13};             //Pines de las filas
 
 
 int error;
@@ -24,64 +39,85 @@ int claveUsuario[4];
 int i;
 int claveEscrita[4];
 char tecla;
+int enciende=1;
 
-
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 LiquidCrystal lcd(5, 4, 3, 2, 1, 0);
 
-void despierta(void) {
-}
 
+/*----------------------------------------------------------------------------*/
+/*-                Funcion que borra la pantalla del LCD                     -*/
+/*----------------------------------------------------------------------------*/
 void borraPantalla(){
   lcd.setCursor(0, 0);
-  lcd.print("                ");
+  lcd.print("                ");                          //16 espacios en blanco.
   lcd.setCursor(0, 1);
   lcd.print("                ");
   lcd.setCursor(0, 0);
 }
 
+/*----------------------------------------------------------------------------*/
+/*-                          Setup del arduino                               -*/
+/*----------------------------------------------------------------------------*/
+/*En el setup cargo el vector claveUsuario, ademas de configurar la interrupcion 
+/*software que comprueba si el detector de movimiento por infrarojos se ha activado*/
+
+
 void setup(){
-   Serial.begin(9600);
-   //delay(3000);
+  Serial.begin(9600);
   lcd.begin(16, 2);
-   EEPROM.write(0, 0xF);      //pARA SIMULAR PRIMERA VEZ
-   if(EEPROM.read(0)==0xF){                              //Si la posicion 0 es F, nunca se ha encendido el PIC. La clave es 1 2 3 4.
-      for(i=0; i<4; i++){
-         claveUsuario[i]=clave1234[i];
-         //printf(lcd_putc,"%d ", claveUsuario[i]);
-      }
-   }
-   else{                                                  //Si la posicion 0 es A, se ha cambiado la clave por defecto 1 2 3 4 por otra.
+  //EEPROM.write(0, 0xF);                                 //PARA SIMULAR PRIMERA VEZ, ya que si laposicion uno tiene F nunca se ha encendido.
+  if(EEPROM.read(0)==0xF){                                //Si la posicion 0 es F, nunca se ha encendido el PIC. La clave es 1 2 3 4.
+    for(i=0; i<4; i++){
+      claveUsuario[i]=clave1234[i];
+    }
+  }
+  else{                                                   //Si la posicion 0 es A, se ha cambiado la clave por defecto 1 2 3 4 por otra.
                                                           //Cargo la contraseña de la eeprom.                                                     
-      for(i=0; i<4; i++){  
-         claveUsuario[i]=EEPROM.read(i+1);
-         delay(100);
-         
-      }
-   }
+    for(i=0; i<4; i++){  
+      claveUsuario[i]=EEPROM.read(i+1);
+      delay(100);
+    }
+  }
   
-  
-  
-  // initialize timer1 
-  noInterrupts();           // disable all interrupts
+    
+                                                          // Inicializo el timer1
+  noInterrupts();                                         // desactivo las interrupciones.
   TCCR1A = 0;
   TCCR1B = 0;
 
-  TCNT1 = 34286;            // preload timer 65536-16MHz/256/2Hz
-  //TCCR1B |= (1 << CS12);    
+  TCNT1 = 34286;                                          //Preload timer 65536-16MHz/256/2Hz
   TCCR1B = TCCR1B & 0b11111000 | 0x04;          
-  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
-  interrupts();             // enable all interrupts
+  TIMSK1 |= (1 << TOIE1);                                 // habilito la interrupcion por overflow del timer1
+  interrupts();                                           // habilito todas las interrupciones.
   
 }
 
 
-ISR(TIMER1_OVF_vect){
+ISR(TIMER1_OVF_vect){                                     //La interrupcion asociada al timer1 ejecuta compruebaPIR();
   compruebaPIR();
 }
 
 
 
+
+/*----------------------------------------------------------------------------*/
+/*-                          compruebaPIR                               -*/
+/*----------------------------------------------------------------------------*/
+/*La patilla A0 tiene una resistencia que fuerza a baja el voltaje, si se 
+/*detecta un ladron el PIR pone u voltaje de 0.3 (V), por lo que 0.3*255 es
+/*igual a 76.5. Por seguridad he puesto que se active si existe una entrada
+/*analogica de mas de 60*/
+
 void compruebaPIR(){
+  if(enciende){
+    digitalWrite(A5, HIGH);
+    enciende=0;
+  }
+  else{
+    digitalWrite(A5, LOW);
+    enciende=1;
+  }
   if(analogRead(A0)>60){
     borraPantalla();
     lcd.print("LADRON DETECTADO");
@@ -97,14 +133,13 @@ void compruebaPIR(){
 
 
 
-
-
-
-
-
+/*----------------------------------------------------------------------------*/
+/*-                          Loop del Arduino                                -*/
+/*----------------------------------------------------------------------------*/
+/*Funcion principal que ejecuta el sistema de alarma, la tecla A se interpreta 
+/*como una peticion de cambio de pass*/
 
 void loop(){
-  //lcd_send_byte(0, 1); //Borra LCD
   error=0;
   lcd.setCursor(0, 0);
   lcd.print("Pass:           ");
@@ -129,9 +164,9 @@ void loop(){
     lcd.setCursor(0, 0);
     lcd.print("PASS ANTERIOR?:");
     lcd.setCursor(0, 1);
+    
     for(i=0; i<4; i++){
       tecla=0;
-      
       while (tecla==0){
         tecla = keypad.getKey();
       }
@@ -147,42 +182,42 @@ void loop(){
         error=1;
       }
     }
-    if(!error){                                     //Si no hay errores en lo pass cambio 
-    //lcd_send_byte(0, 1); //Borra LCD
     
-    borraPantalla();
-    lcd.print("CLAVE CORRECTA ! ");
-    lcd.print(tecla);
-    delay(1000);
-    //lcd_send_byte(0, 1); //Borra LCD
-    borraPantalla();
-    lcd.print("Nuevo PASS: ");
-    EEPROM.write(0, 0xA);                          //Se ha introducido una clave por primera vez.
-    lcd.setCursor(0, 1);
-    for(i=1; i<5; i++){
-      tecla=0;
-      while (tecla==0){
-        tecla = keypad.getKey();
-      }
-      EEPROM.write(i, tecla-48);
+    if(!error){                                     //Si no hay errores en lo pass cambio 
+      borraPantalla();
+      lcd.print("CLAVE CORRECTA ! ");
       lcd.print(tecla);
-    }
-    delay(1000);
-    borraPantalla();
-    lcd.print("OK, nueva clave");
-    lcd.setCursor(0, 1);
-    lcd.print("definida");
-    delay(2000);
-    borraPantalla();
+      delay(1000);
+      
+      borraPantalla();
+      lcd.print("Nuevo PASS: ");
+      EEPROM.write(0, 0xA);                          //Se ha introducido una clave por primera vez.
+      lcd.setCursor(0, 1);
+      
+      for(i=1; i<5; i++){
+        tecla=0;
+        while (tecla==0){
+          tecla = keypad.getKey();
+        }
+        
+        EEPROM.write(i, tecla-48);
+        lcd.print(tecla);
+      }
+      
+      delay(1000);
+      borraPantalla();
+      lcd.print("OK, nueva clave");
+      lcd.setCursor(0, 1);
+      lcd.print("definida");
+      delay(2000);
+      borraPantalla();
 
-    for(i=0; i<4; i++){
-      claveUsuario[i]=EEPROM.read(i+1);
-      delay(100);
-      //printf(lcd_putc,"%d ", claveUsuario[i]);
+      for(i=0; i<4; i++){
+        claveUsuario[i]=EEPROM.read(i+1);
+        delay(100);
       }
     }
     else{                                           //Si hay errores no hago nada digo que la clave es incorrecta.
-      //lcd_send_byte(0, 1); //Borra LCD
       borraPantalla();
       lcd.print("CLAVE INCORRECTA: ");
       delay(1000);
@@ -195,6 +230,7 @@ void loop(){
         error=1;
       }
     }
+    
     if(!error){
       borraPantalla();
       lcd.print("CLAVE CORRECTA ! ");
@@ -206,10 +242,6 @@ void loop(){
       borraPantalla();
       lcd.print("DESACTIVADA :)");
       while(true);
-      //digitalWrite(A5, LOW);
-      //lcd_send_byte(0, 1); //Borra LCD
-      //lcd.print("zZzZzZzZZZ");
-      //sleep();
     }
     else{
       borraPantalla();
@@ -219,7 +251,4 @@ void loop(){
   }
 }  
   
-
-
-
 
